f0c5aa7c4ba24dc300d3fc9a83b346af1b8549ab
Nguyen Vu Thanh <thanh14704@gmail.com>
2025-08-08 10:55:19 +0700

    Footer and policy update

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import React,{useContext, useState, useEffect} from 'react';
+import Title from './Title';
+import { ShopContext } from '../context/ShopContext';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+    const {products} = useContext(ShopContext);
+    const [bestSellers, setBestSellers] = useState([]);
+
+    useEffect(() => {
+        const filtered = products.filter(item => item.bestseller);
+        setBestSellers(filtered.slice(0, 5));
+    }, []);
+
+  return (
+    <div className='my-10'>
+        <div className="text-center text-3xl py-8">
+            <Title text1={"BEST"} text2={"SELLERS"} />
+            <p className="w-3/4 m-auto text-sx sm:text-sm md:text-base text-gray-600">
+                Discover our best-selling products that customers love.
+            </p>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+            {
+                bestSellers.map((item,index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default BestSeller
